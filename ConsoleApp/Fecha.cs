663a7924fca682aa335c170cd2fa89123a27de68
Alonso <alonsete2000@gmail.com>
2022-09-19 19:46:47 +0200

    Comparacion de fechas terminado

--- ConsoleApp/Fecha.cs
+++ ConsoleApp/Fecha.cs
@@ -18,18 +18,18 @@ namespace ConsoleApp
                 Console.WriteLine("1.Dia Semana");
                 Console.WriteLine("2.Incrementar fecha");
                 Console.WriteLine("3.Diferencia fechas");
-                Console.WriteLine("4.Salir");
+                Console.WriteLine("4.Comparar fechas");
                 Console.WriteLine("Elige una opcion: ");
                 opcion = Console.ReadLine();
                 resultado = Int32.Parse(opcion);
             } while (resultado < 1 || resultado > 4);
-            String d1;
-            String m1;
-            String a1;
-            String i;
-            String d2;
-            String m2;
-            String a2;
+            string d1;
+            string m1;
+            string a1;
+            string i;
+            string d2;
+            string m2;
+            string a2;
             switch (resultado)
             {
                 case 1:
@@ -113,7 +113,52 @@ namespace ConsoleApp
                     }
                     menuFechas(); ;
                     break;
-                case 4: ;
+                case 4:
+                    try
+                    {
+                        Console.WriteLine("PRIMERA FECHA");
+                        Console.WriteLine("Escribe el año: ");
+                        a1 = Console.ReadLine();
+                        int anio = Int32.Parse(a1);
+                        Console.WriteLine("Escribe el mes: ");
+                        m1 = Console.ReadLine();
+                        int mes = Int32.Parse(m1);
+                        Console.WriteLine("Escribe el dia: ");
+                        d1 = Console.ReadLine();
+                        int dia = Int32.Parse(d1);
+
+                        Console.WriteLine("SEGUNDA FECHA");
+                        Console.WriteLine("Escribe el año: ");
+                        a2 = Console.ReadLine();
+                        int anioo = Int32.Parse(a2);
+                        Console.WriteLine("Escribe el mes: ");
+                        m2 = Console.ReadLine();
+                        int mess = Int32.Parse(m2);
+                        Console.WriteLine("Escribe el dia: ");
+                        d2 = Console.ReadLine();
+                        int diaa = Int32.Parse(d2);
+                        DateTime date1 = new DateTime(anio, mes, dia);
+                        Console.WriteLine(date1.ToString("dd/MM/yyyy"));
+                        DateTime date2 = new DateTime(anioo, mess, diaa);
+                        Console.WriteLine(date2.ToString("dd/MM/yyyy"));
+                        int comparacion = date1.CompareTo(date2);
+                        if(comparacion < 0)
+                        {
+                            Console.WriteLine(date1.ToString("dd/MM/yyyy")+" es menor que "+date2.ToString("dd/MM/yyyy"));
+                        }else if(comparacion == 0)
+                        {
+                            Console.WriteLine(date1.ToString("dd/MM/yyyy") + " son iguales "+ date2.ToString("dd/MM/yyyy"));
+                        }
+                        else
+                        {
+                            Console.WriteLine(date1.ToString("dd/MM/yyyy") + " es mayor que "+date2.ToString("dd/MM/yyyy"));
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Ese mes no tiene ese dia");
+                    };
                     break;
             }
         }
