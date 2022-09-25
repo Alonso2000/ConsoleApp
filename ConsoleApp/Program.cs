1db4163e3a3c68e55ed5b4ef79c0946676ca344f
Alonso <alonsete2000@gmail.com>
2022-09-25 16:29:47 +0200

    Acabado modificacion yeliminacion

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -22,71 +22,8 @@ public class Program
             case 1: Fecha a = new Fecha();
                 a.menuFechas();
                 break;
-            case 2:
-                string n = "";
-                int nu = 0;
-                string titulo = "";
-                string autor = "";
-                string estilo = "";
-                string editorial = "";
-                ArrayList libros = new ArrayList();
-                bool acabado = false;
-                do
-                {
-                    Console.WriteLine("1.Insertar libro");
-                    Console.WriteLine("2.Ver listado de libros");
-                    Console.WriteLine("3.Modificar algun libro de los ya añadidos");
-                    Console.WriteLine("4.Eliminar algun libro de los ya añadidos");
-                    Console.WriteLine("5.Salir");
-                    n = Console.ReadLine();
-                    nu = Int32.Parse(n);
-
-                switch (nu)
-                {
-                    case 1:
-                        Console.WriteLine("Escribe el titulo:");
-                        titulo = Console.ReadLine();
-                        Console.WriteLine("Escribe el autor:");
-                        autor = Console.ReadLine();
-                        Console.WriteLine("Escribe el estilo");
-                        estilo = Console.ReadLine();
-                        Console.WriteLine("Escribe la editorial");
-                        editorial = Console.ReadLine();
-                        Libro lib = new Libro(titulo, autor, estilo, editorial);
-                        libros.Add(lib);
-                        break;
-
-                    case 2:
-                        for(int i = 0; i < libros.Count; i++)
-                        {
-                                Console.WriteLine(libros[i]);
-                        }
-                        break;
-                    case 3:
-                          Console.WriteLine("¿Que libro quieres modificar?");
-                          titulo = Console.ReadLine();
-                          for(int i = 0; i< libros.Count; i++)
-                          {
-                              if (libros[i] == titulo)
-                              {
-
-                              }
-                          }
-                          break;
-                        case 4:
-                            Console.WriteLine("¿Que libro quieres eliminar?");
-                            titulo = Console.ReadLine();
-                            for(int i = 0;i < libros.Count; i++)
-                            {
-                                if (libros[0])
-                                {
-                                    libros.RemoveAt(i);
-                                }
-                            }
-                            break;
-
-                }
-                } while (nu < 1 || nu > 6 || acabado == false);
+            case 2: Libro b = new Libro();
+                b.menuLibros();
                 break;
             case 3:
                 ;
