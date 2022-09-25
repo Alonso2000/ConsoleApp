e0cf40ab17969a914315823abed085062b849429
Alonso <alonsete2000@gmail.com>
2022-09-25 17:37:59 +0200

    Creqando la clase deportivo

--- ConsoleApp/Velero.cs
+++ ConsoleApp/Velero.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    internal class Velero
+    public class Velero : Barco
     {
+        public int num_mastiles = 0;
+
+        public Velero(string matricula, int eslora, int anio_fabricacion, int mastiles)
+        {
+            this.matricula = matricula;
+            this.eslora = eslora;
+            this.anio_fabricacion = anio_fabricacion; 
+            this.num_mastiles = mastiles;
+        }
+
+        public int Mastiles
+        {
+            get { return num_mastiles; }
+            set { num_mastiles = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{{}}";
+        }
     }
 }
